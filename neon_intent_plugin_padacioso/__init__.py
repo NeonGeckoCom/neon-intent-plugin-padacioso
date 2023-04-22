0bc5ad835b222294c0a682641443c7f199db84cc
JarbasAi <jarbasai@mailfence.com>
2023-04-22 11:15:11 +0100

    dataclazz munge/unmunge

--- neon_intent_plugin_padacioso/__init__.py
+++ neon_intent_plugin_padacioso/__init__.py
@@ -3,6 +3,14 @@ from ovos_plugin_manager.intents import IntentExtractor, IntentPriority, IntentD
 from padacioso import IntentContainer
 
 
+def _munge(name, skill_id):
+    return f"{name}:{skill_id}"
+
+
+def _unmunge(munged):
+    return munged.split(":", 2)
+
+
 class PadaciosoExtractor(IntentExtractor):
 
     def __init__(self, config=None,
@@ -19,23 +27,26 @@ class PadaciosoExtractor(IntentExtractor):
             self.engines[lang] = IntentContainer()
         return self.engines[lang]
 
-    def detach_intent(self, intent_name):
+    def detach_intent(self, skill_id, intent_name):
         for intent in self.registered_intents:
+            if intent.skill_id != skill_id:
+                continue
             if intent.name == intent_name and intent.lang in self.engines:
-                self.engines[intent.lang].remove_intent(intent_name)
+                self.engines[intent.lang].remove_intent(_munge(intent_name, skill_id))
         super().detach_intent(intent_name)
 
-    def register_entity(self, entity_name, samples=None, lang=None):
+    def register_entity(self, skill_id, entity_name, samples=None, lang=None):
         lang = lang or self.lang
+        super().register_entity(skill_id, entity_name, samples, lang)
         container = self._get_engine(lang)
-        super().register_entity(entity_name, samples, lang)
         samples = samples or [entity_name]
         container.add_entity(entity_name, samples)
 
-    def register_intent(self, intent_name, samples=None, lang=None):
+    def register_intent(self, skill_id, intent_name, samples=None, lang=None):
         lang = lang or self.lang
+        super().register_intent(skill_id, intent_name, samples, lang)
+        intent_name = _munge(intent_name, skill_id)
         container = self._get_engine(lang)
-        super().register_intent(intent_name, samples, lang)
         container.add_intent(intent_name, samples)
 
     def calc_intent(self, utterance, min_conf=0.5, lang=None, session=None):
@@ -44,8 +55,10 @@ class PadaciosoExtractor(IntentExtractor):
         utterance = utterance.strip().lower()
         intent = container.calc_intent(utterance)
         if intent["name"]:
+            intent_type, skill_id = _unmunge(intent["intent_type"])
+
             for intent in self.registered_intents:
-                if intent.name == intent["name"]:
+                if intent.name == intent_type and intent.skill_id == skill_id:
                     remainder = intent.get_utterance_remainder(utterance)
                     break
             else:
@@ -62,9 +75,9 @@ class PadaciosoExtractor(IntentExtractor):
             ratio += 0.01
             ratio = ratio / len(self.segmenter.segment(utterance))
             intent["conf"] = 1 - ratio
-            skill_id = self.get_intent_skill_id(intent["intent_type"])
+
             return IntentMatch(intent_service=intent["intent_engine"],
-                               intent_type=intent["intent_type"],
+                               intent_type=intent_type,
                                intent_data=intent,
                                confidence=intent["conf"],
                                skill_id=skill_id)
